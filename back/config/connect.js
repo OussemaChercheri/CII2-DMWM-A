994f9a47fc215cfcf0b183442644d0262812f90c
Ons <chebbions7@gmail.com>
2024-02-29 14:12:26 +0100

    connection to BD

---
+++ back/config/connect.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+const uri ="mongodb+srv://Ons:TzhMrjjB3PTFKWNk@backenddb.3yansug.mongodb.net/?retryWrites=true&w=majority&appName=BackendDb";
+mongoose.connect(uri)
+.then(
+    //fi westha arrow function bch wa9t t5dm todhher w catch bch ken famma error
+    ()=>{
+        console.log('connected successufully');
+    }
+)
+.catch(
+    (err)=>{
+        console.error('Failed to connect:', err);
+    }
+)
+
+module.exports=mongoose;
--- back/server.js
+++ back/server.js
@@ -1,3 +1,15 @@
 const express=require('express');
 const app =express();
+require('./config/connect'); 
+const port=3000;
 
+
+
+
+app.listen(port, (error) => {
+    if (error) {
+        console.error('Failed to start the server:', error);
+    } else {
+        console.log(`App is running on port ${port}`);
+    }
+});
