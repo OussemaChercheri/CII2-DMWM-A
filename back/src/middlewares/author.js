703f72f4f78d3fcd52ebdd4ce82f92c762286ea6
OnsChebi <chebbions7@gmail.com>
2024-04-24 17:39:17 +0100

    authorization middleware

---
+++ back/src/middlewares/author.js
@@ -0,0 +1,13 @@
+const Auhtorization =(authorized)=>{
+    const role=req.body.role;
+    return (req,res,next)=>{
+        if ( authorized.includes(role)){
+            next();
+        }else{
+            return res.status(401).json("you're not authorized");
+        }
+    }
+
+}
+
+module.exports=Auhtorization;
